5898c6e26f265127277094e20aee75a78dab8c75
sricharankrishnan <sricharan.krishnan@gmail.com>
2026-01-28 11:07:07 +0530

    [feat] Hero Section
    - created a fixed jumbotron section that would be a sticky while the
      website scrolls
    - created a hero section with title, sub-text with links to the NPM page
      and github repo page
    - stylesheet details applied to both sections responsive

--- assets/js/home/index.js
+++ assets/js/home/index.js
@@ -2,8 +2,23 @@
   /* top level vars */
   var hasWebpSupport = false;
 
+  function createdGeometricHeroVideo() {
+    var pe = $("#geometricFixedJumbotron");
+    var config = {
+      parentElement: pe,
+      playInMobile: true,
+      playInTablet: true,
+      playInDesktop: true,
+      webmVideo: pe.attr("data-webm"),
+      mp4Video: pe.attr("data-mp4"),
+      callback: function() {},
+      fallbackImage: ""
+    };
+    buildHtmlVideo(config);
+  }
+
   function pageControl() {
-    console.log("Hello World - Home Page Loaded.");
+    createdGeometricHeroVideo();
   }
 
   $(d).ready(function() {

14937a19fa8823e08a411665f2d1ecb1026a8fe8
sricharankrishnan <sricharan.krishnan@gmail.com>
2026-01-28 16:51:09 +0530

    [feat] Footer
    - created section that basically describes how the package works with an
      intersection observer browser api loading a video on 10% threshold
    - footer section of the website created that contains keyword
      description and links/attributes

--- assets/js/home/index.js
+++ assets/js/home/index.js
@@ -17,8 +17,44 @@
     buildHtmlVideo(config);
   }
 
+  function handleSkullVideo() {
+    var target = $("#skullVideoParent");
+
+    /* do not do this if not there */
+    if (!target || !("IntersectionObserver" in window)) {
+      return;
+    } else {
+      var observer = new IntersectionObserver(
+        function (entries, observerInstance) {
+          entries.forEach(function(entry) {
+            if (entry.isIntersecting && entry.intersectionRatio >= 0.1) {
+              observerInstance.unobserve(entry.target); // trigger once
+              var config = {
+                parentElement: target,
+                playInMobile: true,
+                playInTablet: true,
+                playInDesktop: true,
+                webmVideo: target.attr("data-webm"),
+                mp4Video: target.attr("data-mp4"),
+                callback: function() {},
+                fallbackImage: ""
+              }
+              buildHtmlVideo(config);
+            }
+          });
+        },
+        {
+          threshold: 0.1
+        }
+      );
+      /* start observation */
+      observer.observe(target[0]);
+    }
+  }
+
   function pageControl() {
     createdGeometricHeroVideo();
+    handleSkullVideo();
   }
 
   $(d).ready(function() {
